a412e902ed14b02b6030580f2d6414703ec8889e
FelipeSnow <felipenbrito18@outlook.com>
2021-10-01 18:45:42 -0400

    .

--- lib/screens/MapScreen/map_screen.dart
+++ lib/screens/MapScreen/map_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+// The widget that renderize the map on the app :)
+
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
 
